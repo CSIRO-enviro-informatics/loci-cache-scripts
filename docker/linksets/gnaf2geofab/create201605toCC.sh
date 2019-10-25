6d3838d34eee8883dd4e0e467c87f98835392021
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-10-25 14:50:27 +1100

    Update the compose files to supply appropriate parameters for different GNAF versions

--- docker/linksets/gnaf2geofab/create201605toCC.sh
+++ docker/linksets/gnaf2geofab/create201605toCC.sh
@@ -3,4 +3,4 @@ set -e
 
 source ../../../common/common.sh
 
-docker-compose -f docker-compose.base.yml -f docker-compose.2016.yml up --build --force-recreate
+docker-compose -f docker-compose.base.yml -f docker-compose.201605.yml up --build --force-recreate
